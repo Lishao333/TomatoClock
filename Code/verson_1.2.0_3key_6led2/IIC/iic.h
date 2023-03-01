99ba5c95679afbcfd96d457c63e32838f805d953
Lishao333 <1464026485@qq.com>
2023-03-01 20:26:49 +0800

    删除备份

--- Code/verson_1.2.0_3key_6led2/IIC/iic.h
+++
@@ -1,8 +0,0 @@
-#ifndef _IIC_H_
-#define _IIC_H_
-
-#include "gd32f10x.h"
-
-void HW_I2cInit(void);
-
-#endif
--- Code/verson_1.2.0_3key_6led2/Uart/uart.h
+++
@@ -1,5 +0,0 @@
-#ifndef __UART_H
-#define __UART_H
-
-void pc_com_init(void);
-#endif /* __UART_H */
